ba8da7a091696d671452e87f870e57c32a7ab480
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-12-05 13:16:30 +0500

    Dashboard UI update

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,23 +5,23 @@ import { CiLocationOn } from 'react-icons/ci';
 
 export const BottomNav = ({ active }) => {
   return (
-    <div className='btm-nav'>
+    <div className='btm-nav border-none'>
       <Link
         to={'/'}
-        className={!active || active === 'dashboard' ? 'active' : ''}
+        className={!active || active === 'dashboard' ? 'bg-blue-500 text-white' : 'bg-blue-50'}
       >
         <FaTachometerAlt className='h-5 w-5' />
       </Link>
       <Link
         to={'/reports'}
-        className={!active || active === 'reports' ? 'active' : ''}
+        className={!active || active === 'reports' ? 'bg-blue-500 text-white' : 'bg-blue-50'}
       >
         <FaFile className='h-5 w-5' />
       </Link>
       {localStorage.getItem('@type') !== 'halqa' && (
         <Link
           to={'/comparison'}
-          className={!active || active === 'comparison' ? 'active' : ''}
+          className={!active || active === 'comparison' ? 'bg-blue-500 text-white' : 'bg-blue-50'}
         >
           <BsFiles className='h-5 w-5' />
         </Link>
@@ -29,7 +29,7 @@ export const BottomNav = ({ active }) => {
       {['province', 'maqam', 'division'].includes(localStorage.getItem('@type')) && (
         <Link
           to={'/locations'}
-          className={!active || active === 'locations' ? 'active' : ''}
+          className={!active || active === 'locations' ? 'bg-blue-500 text-white' : 'bg-blue-50'}
         >
           <CiLocationOn className='h-5 w-5' />
         </Link>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { GeneralLayout } from '../components';
 import instance from '../api/instrance';
 import { useEffect } from 'react';
+import { FaLocationDot } from 'react-icons/fa6';
+import { CiLocationOn } from 'react-icons/ci';
+import { FaLocationArrow } from 'react-icons/fa';
 
 export const Dashboard = () => {
   const [count, setCount] = useState(0);
@@ -48,24 +51,126 @@ export const Dashboard = () => {
     getAllReports();
   }, []);
   return (
-    <GeneralLayout title={"Dashboard"} active={'dashboard'}>
-      <div className='relative flex lg:flex-row flex-col gap-3 items-center p-5 justify-center h-[calc(100vh-65.6px-64px)]'>
-        <div className='p-5 w-full bg-slate-200 rounded-xl'>
-          <h1 className='font-bold text-lg'>Total Reports</h1>
-          <h2 className='underline text-2xl'>
-            {loading ? (
-              <span className='loading loading-ring loading-md text-blue-400'></span>
-            ) : (
-              `${count} Report(s)`
-            )}
-          </h2>
-        </div>
-        {localStorage.getItem('@type') !== 'province' && (
+    <GeneralLayout title={'Dashboard'} active={'dashboard'}>
+      <div className='relative flex w-full gap-3 items-center p-5 justify-center h-[calc(100vh-65.6px-64px)] overflow-hidden overflow-y-scroll bg-blue-50'>
+        {/* <div className='grid grid-cols-2 w-full gap-3'>
+          <div className='p-5 w-full bg-slate-200 rounded-xl'>
+            <h1 className='font-bold text-lg'>Total Reports</h1>
+            <h2 className='underline text-2xl'>
+              {loading ? (
+                <span className='loading loading-ring loading-md text-blue-400'></span>
+              ) : (
+                `${count} Report(s)`
+              )}
+            </h2>
+          </div>
           <div className='p-5 w-full bg-slate-200 rounded-xl'>
             <h1 className='font-bold text-lg'>Next Report:</h1>
-            <h2 className='underline text-2xl'>December 01, 2023</h2>
+            <h2 className='underline text-2xl'>
+              {moment('January 01, 2024')
+                .fromNow()
+                .toString()
+                .split('')
+                .map((_, index) => (index === 0 ? _.toUpperCase() : _))
+                .join('')}
+            </h2>
           </div>
-        )}
+          <div className='p-5 w-full bg-slate-200 rounded-xl'>
+            <h1 className='font-bold text-lg'>Total Divisions:</h1>
+            <h2 className='underline text-2xl'>2 Division(s)</h2>
+          </div>
+          <div className='p-5 w-full bg-slate-200 rounded-xl'>
+            <h1 className='font-bold text-lg'>Total Maqams:</h1>
+            <h2 className='underline text-2xl'>2 Maqam(s)</h2>
+          </div>
+          <div className='p-5 w-full bg-slate-200 rounded-xl'>
+            <h1 className='font-bold text-lg'>Total Units:</h1>
+            <h2 className='underline text-2xl'>2 Unit(s)</h2>
+          </div>
+          <div className='p-5 w-full bg-slate-200 rounded-xl'>
+            <h1 className='font-bold text-lg'>Total Nazims:</h1>
+            <h2 className='underline text-2xl'>2 Nazim(s)</h2>
+          </div>
+        </div> */}
+        <div className='grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-4 sm:px-8 w-full'>
+          <div className='flex items-center bg-white border rounded-sm overflow-hidden shadow'>
+            <div className='p-4 bg-green-400'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-12 w-12 text-white'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'
+                ></path>
+              </svg>
+            </div>
+            <div className='px-4 text-gray-700'>
+              <h3 className='text-sm tracking-wider'>Total Nazims</h3>
+              <p className='text-3xl'>12,768</p>
+            </div>
+          </div>
+          <div className='flex items-center bg-white border rounded-sm overflow-hidden shadow'>
+            <div className='p-4 bg-blue-400'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-12 w-12 text-white'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2'
+                ></path>
+              </svg>
+            </div>
+            <div className='px-4 text-gray-700'>
+              <h3 className='text-sm tracking-wider'>Total Reports</h3>
+              <p className='text-3xl'>
+                {loading ? (
+                  <span className='loading loading-bars loading-md'></span>
+                ) : (
+                  `${count} Report(s)`
+                )}
+              </p>
+            </div>
+          </div>
+          <div className='flex items-center bg-white border rounded-sm overflow-hidden shadow'>
+            <div className='p-4 bg-indigo-400'>
+              <CiLocationOn className='w-12 h-12 text-white' />
+            </div>
+            <div className='px-4 text-gray-700'>
+              <h3 className='text-sm tracking-wider'>Total Maqams</h3>
+              <p className='text-3xl'>142,334</p>
+            </div>
+          </div>
+          <div className='flex items-center bg-white border rounded-sm overflow-hidden shadow'>
+            <div className='p-4 bg-red-400'>
+              <FaLocationDot className='h-12 w-12 text-white' />
+            </div>
+            <div className='px-4 text-gray-700'>
+              <h3 className='text-sm tracking-wider'>Total Division</h3>
+              <p className='text-3xl'>34.12%</p>
+            </div>
+          </div>
+          <div className='flex items-center bg-white border rounded-sm overflow-hidden shadow'>
+            <div className='p-4 bg-red-400'>
+              <FaLocationArrow className='w-12 h-12 text-white' />
+            </div>
+            <div className='px-4 text-gray-700'>
+              <h3 className='text-sm tracking-wider'>Total Units</h3>
+              <p className='text-3xl'>34.12%</p>
+            </div>
+          </div>
+        </div>
       </div>
     </GeneralLayout>
   );
