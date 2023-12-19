56842bdbdc024ccf037fc9f9272503046e8c9003
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-12-19 16:26:06 +0500

    Updated API endpoint

--- src/api/instrance.js
+++ src/api/instrance.js
@@ -3,7 +3,7 @@ const env = process.env.REACT_APP_ENV || 'production';
 const instance = axios.create({
   baseURL:
     env === 'production'
-      ? 'http://192.168.0.117:5000/api/v1'
+      ? 'https://api.jamiatreporting.com/api/v1'
       : 'http://localhost:5000/api/v1',
   headers: {
     'Content-Type': 'application/json', // Set default headers
