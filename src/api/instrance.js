30b0e82599c65341e4ebe7630dd48c0eb17f7f10
Shuja Ur Rehman <shuja0094@gmail.com>
2024-01-09 17:30:47 +0500

    still working

--- src/api/instrance.js
+++ src/api/instrance.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+const env = process.env.REACT_APP_ENV || 'production';
 const instance = axios.create({
-  baseURL: 'https://apiv2.jamiatreporting.com/api/v1',
-  // baseURL: 'http://192.168.0.110:5000/api/v1',
+  baseURL:
+    env === 'production'
+      ? 'http://192.168.0.117:5000/api/v1'
+      : 'http://localhost:5000/api/v1',
   headers: {
     'Content-Type': 'application/json', // Set default headers
     // Add other default headers if needed
