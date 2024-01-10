ddf7edd5cfc61bd7f5e5add5347d304c7f7e2183
Shuja Ur Rehman <shuja0094@gmail.com>
2024-01-10 13:22:14 +0500

    search done

--- src/utils.js
+++ src/utils.js
@@ -8,19 +8,19 @@ export const toJson = (formData) => {
 
 export const convertDataFormat = (data) => {
   function processKey(key) {
-    const parts = key.split('-');
+    const parts = key.split("-");
     if (parts.length === 1) {
       return key;
     }
 
     const section = parts[0];
-    const attribute = parts.slice(1).join('-');
+    const attribute = parts.slice(1).join("-");
 
     if (!result[section]) {
       result[section] = {};
     }
 
-    if (attribute === 'registered') {
+    if (attribute === "registered") {
       result[section][attribute] = data[key] ? true : false;
     } else {
       result[section][attribute] = data[key];
@@ -46,31 +46,31 @@ export const reverseDataFormat = (data) => {
   Object.keys(data || {}).forEach((i) => {
     if (!data[i]) {
       obj[i] = null;
-    } else if (typeof data[i] === 'object') {
+    } else if (typeof data[i] === "object") {
       Object.keys(data[i])
-        .filter((j) => j !== '_id' && j !== '__v')
+        .filter((j) => j !== "_id" && j !== "__v")
         .forEach((j) => {
           if (!data[i][j]) {
             obj[j] = null;
-          } else if (typeof data[i][j] === 'object') {
+          } else if (typeof data[i][j] === "object") {
             Object.keys(data[i][j])
-              .filter((k) => k !== '_id' && k !== '__v')
+              .filter((k) => k !== "_id" && k !== "__v")
               .forEach((k) => {
-                if (i === 'mentionedActivityId' && j === 'studyCircle') {
-                  obj[j + 'Mentioned-' + k] = data[i][j][k];
-                } else if (i === 'tdId' && j === 'registered') {
-                  obj['registeredTosee'] = data[i][j][k];
+                if (i === "mentionedActivityId" && j === "studyCircle") {
+                  obj[j + "Mentioned-" + k] = data[i][j][k];
+                } else if (i === "tdId" && j === "registered") {
+                  obj["registeredTosee"] = data[i][j][k];
                 } else {
-                  obj[j + '-' + k] = data[i][j][k];
+                  obj[j + "-" + k] = data[i][j][k];
                 }
               });
           } else {
-            if (i === 'tdId' && j === 'registered') {
-              obj['registeredTosee'] = data[i][j];
-            } else if (i === 'halqaLibId' && j === 'registered') {
-              obj['registeredLibrary'] = data[i][j];
-            } else if (i === 'wiId' && j === 'registered') {
-              obj['registeredWorker'] = data[i][j];
+            if (i === "tdId" && j === "registered") {
+              obj["registeredTosee"] = data[i][j];
+            } else if (i === "halqaLibId" && j === "registered") {
+              obj["registeredLibrary"] = data[i][j];
+            } else if (i === "wiId" && j === "registered") {
+              obj["registeredWorker"] = data[i][j];
             } else {
               obj[j] = data[i][j];
             }
@@ -85,3 +85,4 @@ export const reverseDataFormat = (data) => {
   }
   return obj;
 };
+
