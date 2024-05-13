26e17c8a6a65a7834afccd61d216ea75e54df905
Shuja Ur Rehman <71079854+sabeeharif@users.noreply.github.com>
2024-05-13 19:59:32 +0500

    deploy issues

--- src/api/instrance.js
+++ src/api/instrance.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 const env = process.env.REACT_APP_ENV || "production";
 const instance = axios.create({
-  baseURL:
-    env === "production"
-      ? "https://ji-reporting-backend.vercel.app/api/v1/"
-      : "http://localhost:5000/api/v1",
+  baseURL: "https://ji-reporting-backend.vercel.app/api/v1/",
+
   headers: {
     "Content-Type": "application/json", // Set default headers
     // Add other default headers if needed
