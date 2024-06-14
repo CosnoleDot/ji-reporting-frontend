43089c1086dd9723dd6083b0603c7a3de8e09f83
Hassaan Mujtaba <saddiquehari2511@gmail.com>
2024-06-14 11:45:19 +0500

    Comment resolved

--- src/pages/Compilaton/IlaqaCompile.jsx
+++ src/pages/Compilaton/IlaqaCompile.jsx
@@ -64,7 +64,7 @@ export const IlaqaCompile = () => {
       data[i] = 0;
     }
   });
-  console.log(compileReport);
+
   return (
     <GeneralLayout>
       {Object.keys(compileReport).length > 2 ? (
