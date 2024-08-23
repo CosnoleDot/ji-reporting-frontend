fe6d8478f9d620358c867e35b2a0d7df8eab2a5f
Muhammad Abuzar <80649561+Haris-Siddiqui@users.noreply.github.com>
2024-08-23 17:38:44 +0500

    pushing code from 269 to origin main

--- src/api/instrance.js
+++ src/api/instrance.js
@@ -4,7 +4,6 @@ const env = process.env.REACT_APP_ENV || "production";
 
 const instance = axios.create({
   baseURL: "http://localhost:5000/api/v1",
-    
   headers: {
     "Content-Type": "application/json", // Set default headers
     // Add other default headers if needed
